da63c05e31608f2937a128fee783c060fae48221
AfuhFlynns <flyinnsafuh@gmail.com>
2025-01-18 09:56:17 +0100

    Modify log file and added logger

---
+++ src/utils/loger.ts
@@ -0,0 +1,16 @@
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${level.toUpperCase()}] ${timestamp} - ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
+  transports: [
+    new transports.File({ filename: "../logs/log.log" }),
+    new transports.Console(),
+  ],
+});
+
+export default logger;
